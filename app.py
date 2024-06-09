4a12b8f497dd2be28014dba1310ba1a40ad4761b
S3RN4K <149739380+S3RN4K@users.noreply.github.com>
2024-06-09 18:56:40 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,63 +1,29 @@
 from flask import Flask, render_template, request, jsonify
 import whisper
-from processing import improve_and_classify_requirements, improve_requirement, sintetizar_requisito
+from classification import *
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route("/audio", methods=["POST"])
 def audio():
     try:
-        print("Recibido un archivo de audio")  
-
         rec = request.files.get("audio")
         rec.save("test.wav")
-        
-        modelo = whisper.load_model("medium")
+        modelo = whisper.load_model("small")
         file_path = "test.wav"
         transcription = modelo.transcribe(file_path, language="es")
-        
-        print("Texto transcrito:", transcription['text'])
-        
-        improved_requirements, classifications = improve_and_classify_requirements(transcription['text'])
-        
-        print("Requisitos mejorados:", improved_requirements)
-        print("Clasificaciones:", classifications)
-        
-        return jsonify({
-            "transcription": transcription['text'],
-            "improved_requirements": improved_requirements,
-            "classifications": classifications
-        })
-    
+        type_r = classify(transcription['text'])
+        return {"result": "ok", "text": transcription['text'], "type": type_r}
     except Exception as e:
-        print(f"Error procesando el audio: {e}")
-        return jsonify({"error": str(e)}), 500
+        print(e)
+        return {"resultado": "error", "message": str(e)}
 
-@app.route('/mejorar_requisito', methods=['POST'])
-def mejorar_requisito():
-    data = request.get_json()
-    requirement = data.get('requirement')
-    classification = data.get('classification')
 
-    if not requirement:
-        return jsonify({"result": "error", "message": "No se proporcionó un requisito"}), 400
-
-    improved_requirement = improve_requirement(requirement, classification)
-    return jsonify({"result": "ok", "improved_requirement": improved_requirement})
-
-@app.route('/sintetizar_requisito', methods=['POST'])
-def sintetizar_requisito_endpoint():
-    data = request.get_json()
-    requirement = data['requirement']
-    try:
-        sintetizado = sintetizar_requisito(requirement)
-        return jsonify({"result": "ok", "sintetizado": sintetizado})
-    except Exception as e:
-        return jsonify({"result": "error", "message": str(e)}), 500
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
